991ff4030e2be4b5ae56419603425c0a6f5297e8
eshumilova <eshumilova@action-media.ru>
2023-09-18 19:23:52 +0300

    1. Добавлен ещё один Extension, между @Test и @AfterEach. 2. Добавлен ещё один Extension с текстом "Extension: после всех". 3. Добавлен ещё один тэг - tag2.

--- src/test/java/examples/tests/MyTest.java
+++ src/test/java/examples/tests/MyTest.java
@@ -40,7 +40,7 @@ public class MyTest {
     }
 
     @Test
-    @Tag("tag2")
+    @Tag("tag2") //для второго теста добавлен tag2
     void test2() {
         System.out.println("Тест 2");
         QAService qaService = new QAService();
