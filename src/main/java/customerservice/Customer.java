b985a76f6ad5e5a24376f11e250d614a346179f1
Maxime M <mmozgunov@action-media.ru>
2023-11-10 13:54:07 +0300

    Add customer service

---
+++ src/main/java/customerservice/Customer.java
@@ -0,0 +1,39 @@
+package customerservice;
+
+import java.util.UUID;
+
+public class Customer {
+    private UUID id;
+    private String name;
+    private String phone;
+
+    public Customer(UUID id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ src/main/java/customerservice/CustomerService.java
@@ -0,0 +1,35 @@
+package customerservice;
+
+import net.datafaker.Faker;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.IntStream;
+
+import static java.util.UUID.randomUUID;
+
+public class CustomerService {
+    private static final Faker faker = new Faker(Locale.of("RU"));
+    private final List<Customer> customers = new ArrayList<>();
+
+    public List<String> getAllCustomerNames() {
+        return customers.stream().map(Customer::getName).sorted().toList();
+    }
+
+    public List<Customer> getCustomersByPhone(String phone) {
+        return customers.stream().filter(customer -> phone.equals(customer.getPhone())).toList();
+    }
+
+    Customer generateCustomer() {
+        return new Customer(
+                randomUUID(),
+                faker.name().nameWithMiddle(),
+                faker.phoneNumber().phoneNumberInternational()
+        );
+    }
+
+    public CustomerService() {
+        customers.addAll(IntStream.range(0, 25).mapToObj(i -> generateCustomer()).toList());
+    }
+}
